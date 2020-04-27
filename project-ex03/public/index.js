37d81ceba1976cff2e053ca1e76d641b5b0fd034
inhyo <inhyo825@gmail.com>
2020-04-27 13:37:35 +0900

    init commit

--- project-ex03/public/index.js
+++ project-ex03/public/index.js
@@ -1,8 +1,1 @@
-const App = function(){
-    const app = document.createElement('h1');
-    app.textContent = 'Hello World';
-
-    return app;
-}
-
 document.getElementById('root').appendChild(App());
