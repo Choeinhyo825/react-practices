182c1e53d64e6f181db545bb1c0e65325a2b0e2d
inhyo <inhyo825@gmail.com>
2020-05-12 11:21:09 +0900

    kanban-app 깔끔 정리

--- kanban-app/src/App.js
+++ kanban-app/src/App.js
@@ -1,7 +1,8 @@
-
 import React from 'react';
 import KanbanBoard from './KanbanBoard';
 import data from './data.json';
+
+import './App.css';
 
 export default class App extends React.Component {
     render() {
